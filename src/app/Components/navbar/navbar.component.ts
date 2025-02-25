9ee80f782d92c00c84354e70f2f58e5ee79bec22
André Santos <andre16takanho@gmail.com>
2025-02-25 21:19:31 +0000

    Added note! Portfolio in progress

--- src/app/Components/navbar/navbar.component.ts
+++ src/app/Components/navbar/navbar.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import {RouterOutlet} from '@angular/router';
+import {NgIf} from "@angular/common";
 
 @Component({
   selector: 'app-navbar',
   standalone: true,
-  imports: [RouterOutlet],
+    imports: [RouterOutlet, NgIf],
   templateUrl: './navbar.component.html',
   styleUrl: './navbar.component.css'
 })
